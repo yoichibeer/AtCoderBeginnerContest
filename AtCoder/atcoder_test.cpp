7c1f4ff076f78d41a79249a55421000309008993
KODAMA Yoichi <kodama.yoichi@gmail.com>
2019-12-08 21:18:25 +0900

    標準出力をキャプチャして評価

--- AtCoder/atcoder_test.cpp
+++ AtCoder/atcoder_test.cpp
@@ -1,8 +1,21 @@
 ﻿#include <gtest/gtest.h>
 
+#include <iostream>
+#include <array>
+#include <vector>
+
 #include "detect_memory_leak.h"
+
+int a(int, char**)
+{
+    std::cout << "hello";
+    return 0;
+}
 
 TEST(AtCoder, sample)
 {
-    EXPECT_TRUE(true);
+    testing::internal::CaptureStdout();
+    a(0, nullptr);
+    const std::string actual = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(actual, "hello");
 }
